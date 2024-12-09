30cc0ed951729212330a1905b2303ca05624d4a7
hedayatrk30 <hedayatrk30@gmail.com>
2024-12-09 20:19:11 +0530

    Initial commit

---
+++ src/main/java/com/hotelproject/ZHotel/ZHotelApplication.java
@@ -0,0 +1,13 @@
+package com.hotelproject.ZHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotelproject/ZHotel/ZHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelproject.ZHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
